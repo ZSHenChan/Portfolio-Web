f67729ec26afcc357d6091d0600270a902a160b2
ZSHenChan <zishenchan@gmail.com>
2025-05-04 21:35:06 +0800

    chore[/image] downscale image, del[/png, /svg] remove unused icons

--- components/Landing/Project/Hologram.tsx
+++ components/Landing/Project/Hologram.tsx
@@ -9,7 +9,7 @@ function HologramProject() {
   return (
     <ScrollableSection id="hologram">
       <ProjectHeading>Internship - AV Media</ProjectHeading>
-      <ProjectDetail imgSrc="/image/am.jpeg" width={600}>
+      <ProjectDetail imgSrc="/image/hologram.png" width={600}>
         <ProjectHeading className="lg:text-2xl font-bold">
           Interactive Hologram AI Chatbot
         </ProjectHeading>
