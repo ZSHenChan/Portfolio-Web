5c2f83877445cc43d6597a09b2203817497dc754
zishen-chan0 <zshen2002@gmail.com>
2025-05-06 11:52:24 +0800

    style[/page] update landing page UI

---
+++ app/hooks/useMediaQuery.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+export const useMediaQuery = (query: string) => {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const media = window.matchMedia(query);
+      if (media.matches) {
+        setMatches(true);
+      }
+
+      const listener = (event: any) => setMatches(event.matches);
+      media.addEventListener("change", listener);
+
+      return () => media.removeEventListener("change", listener);
+    }
+  }, [query]);
+
+  return matches;
+};
