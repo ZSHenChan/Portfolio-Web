4cec9e71c1b6fadaf3feb1750e5e69610d2594aa
ZSHenChan <zishenchan@gmail.com>
2026-01-31 16:13:16 +0800

    feat(resume-loader): update fetch resume method

--- lib/resume-loader.ts
+++ lib/resume-loader.ts
@@ -1,16 +1,9 @@
 import "server-only";
-import path from "path";
-import { promises as fs } from "fs";
 import { FinalResumeData } from "@/app/interfaces/Resume";
-import { MASTER_RESUME_FILEPATH } from "@/app/lib/chatbot/config";
+import rawResumeData from "@/public/resume/master_data.json";
 
 export async function getMasterResume(): Promise<FinalResumeData> {
-  const jsonDirectory = path.join(process.cwd(), "public");
-  const filePath = path.join(jsonDirectory, MASTER_RESUME_FILEPATH);
-
-  const fileContents = await fs.readFile(filePath, "utf8");
-
-  const data = JSON.parse(fileContents) as FinalResumeData;
+  const data = rawResumeData as unknown as FinalResumeData;
 
   if (!data.header || !data["Work Experiences & Internships"]) {
     throw new Error("Invalid Resume JSON structure");
